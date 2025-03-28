ee8414ed19fe0447b270edb176f08ca99aba3a5a
RocketMafxcker <nuttanan.jatu@bumail.net>
2025-03-29 01:54:03 +0700

    day save

---
+++ Assets/Scripts/DZone.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DZone : MonoBehaviour
+{
+    void Start()
+    {
+        DZone dZone = GetComponent<DZone>();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,70 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    Rigidbody rb;
+    float moveSpeed = 1000f;
+    float flyUpSpeed = 15;
+    float yawPower = 5;
+    float rollPower = 2;
+    bool checkZone = true;
 
-    // Update is called once per frame
-    void Update()
+    Transform target;
+    Vector3 offset;
+    float followSpeed = 20f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = 10f;
+        checkZone = true;
+        target = GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        if (checkZone)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(Vector3.forward * moveSpeed * Time.deltaTime);
+                moveSpeed++;
+                followSpeed++;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.transform.Translate(transform.up * flyUpSpeed * Time.deltaTime);
+                rb.AddForce(transform.up * flyUpSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                rb.AddForce(Vector3.down * flyUpSpeed * Time.deltaTime);
+            }
+            float yaw = Input.GetAxis("Horizontal") * yawPower;
+            float roll = Input.GetAxis("Roll") * rollPower;
+
+            rb.AddTorque(transform.forward * yaw);
+            rb.AddTorque(transform.up * roll);
+        }
+
+        if (checkZone)
+        {
+            Vector3 targetPosition = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        } 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit!!!");
+        if (other.CompareTag("NoMoveZone"))
+        {
+            checkZone = false;
+
+        }
+        if (other.CompareTag("DZone"))
+        {
+            Destroy(gameObject);
+            
+        }
     }
 }
